2901286c68113b6c0cecf7ac6579b998094f1843
Rishant Dwivedi <Rishant Dwivedi>
2015-07-02 22:36:40 -0500

    fix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 		URL: process.env.MONGOLAB_URI || "localhost"
 	},
 	MANDRILL: {
-		KEY: process.env.MANDRILL_KEY || "XDOlGM4j9OjryLJ_qqwOOw"
+		KEY: process.env.MANDRILL_APIKEY || "XDOlGM4j9OjryLJ_qqwOOw"
 	},
 	PORT: process.env.PORT || 5000
 }
